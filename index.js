1ae8fafaad898dbebf78f0d035796f466fb7ed25
Benjamin <ben.chesser19@gmail.com>
2023-04-04 19:15:00 -0700

    fresh start on pong using canvas

---
+++ index.js
@@ -0,0 +1,21 @@
+let canvas = document.getElementById("pong-canvas");
+
+let context = canvas.getContext("2d");
+
+const window_height = 600;
+const window_width = 750;
+
+canvas.width = window_width;
+canvas.height = window_height;
+
+canvas.style.background = "#ffc107";
+
+class Bumper {
+    constructor(){
+
+    }
+}
+
+
+
+
